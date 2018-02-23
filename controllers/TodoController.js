5cbb7cb618a96c0d4fb2427abcb5b676bb6dd801
Reuben Ayres <ra@sent.com>
2018-02-23 11:31:27 -0600

    index/list appears

--- controllers/TodoController.js
+++ controllers/TodoController.js
@@ -9,6 +9,7 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', (req, res) => {
+	console.log(req.body);
 	Todo.create(req.body, (err, createdTodo) => {
 		res.json(createdTodo);	
 	})
--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,49 @@
 console.log("got some js")
 const app = angular.module('MyApp', []);
 
-app.controller('MyController', [function() {
+// added the $http service
+app.controller('MyController', ['$http', function($http) {
 	this.msg ="test msg from the controllerrrr"
+	this.complete = false;
+	const controller = this;
+
+	this.createTodo = function() {
+
+		// grab a promise
+		const makeAjaxCreateCall = $http({
+			method: 'POST',
+			url: '/todo', // 'todos'
+			data: {
+				description: this.description,
+				complete: this.complete
+			}
+		})
+
+		// how to resolve the promise 
+		makeAjaxCreateCall.then(function(res) {
+			// console.log(res.data);
+			// console.log(this)
+			controller.description = '';
+			controller.complete = false
+		}, function(error) {
+			console.error(error)
+		})
+
+	}
+
+	this.getTodos = function() {
+		$http({
+			method: 'GET', 
+			url: '/todo' // todos
+		}).then(function(res) {
+			console.log(res.data)//<--
+			controller.todos = res.data
+		}, function(err) {
+			console.error(err)
+		})
+	}
+	this.getTodos() // run funcction aimmediately 
+
+
+
 }])
